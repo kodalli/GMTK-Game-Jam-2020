80767b121841ed4f72158aa43f54e965efd04bfa
Yologuri <51071067+Yologuri@users.noreply.github.com>
2020-07-11 13:09:42 -0400

    Better clouds

--- Assets/Scripts/backgroundLoop.cs
+++ Assets/Scripts/backgroundLoop.cs
@@ -10,27 +10,29 @@ public class backgroundLoop : MonoBehaviour
     public GameObject[] levels;
     private Camera mainCamera;
     private Vector2 screenBounds;
+    public float choke;
+    public float scrollSpeed;
 
     void Start()
     {
         mainCamera = gameObject.GetComponent<Camera>();
         screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
-        foreach(GameObject obj in levels)
+        foreach (GameObject obj in levels)
         {
             loadChildObjects(obj);
         }
     }
     void loadChildObjects(GameObject obj)
     {
-        float objectWidth = obj.GetComponent<SpriteRenderer>().bounds.size.x;
+        float objectWidth = obj.GetComponent<SpriteRenderer>().bounds.size.x - choke;
         int childsNeeded = (int)Mathf.Ceil(screenBounds.x * 2 / objectWidth);
         GameObject clone = Instantiate(obj) as GameObject;
-        for(int i = 0; i<= childsNeeded; i++)
+        for (int i = 0; i <= childsNeeded; i++)
         {
             GameObject c = Instantiate(clone) as GameObject;
             c.transform.SetParent(obj.transform);
             c.transform.position = new Vector3(objectWidth * i, obj.transform.position.y, obj.transform.position.z);
-            c.name = obj.name + i; 
+            c.name = obj.name + i;
         }
         Destroy(clone);
         Destroy(obj.GetComponent<SpriteRenderer>());
@@ -38,29 +40,38 @@ public class backgroundLoop : MonoBehaviour
     void repositionChildObjects(GameObject obj)
     {
         Transform[] children = obj.GetComponentsInChildren<Transform>();
-        if(children.Length > 1)
+        if (children.Length > 1)
         {
             GameObject firstChild = children[1].gameObject;
             GameObject lastChild = children[children.Length - 1].gameObject;
-            float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x;
-            if(transform.position.x + screenBounds.x > lastChild.transform.positon.x + halfObjectWidth)
+            float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x - choke;
+            if (transform.position.x + screenBounds.x > lastChild.transform.position.x + halfObjectWidth)
             {
                 firstChild.transform.SetAsLastSibling();
                 firstChild.transform.position = new Vector3(lastChild.transform.position.x + halfObjectWidth * 2, lastChild.transform.position.y, lastChild.transform.position.z);
-
             }
-            else if(transform.position.x - screenBounds.x < firstChild.transform.position.x - halfObjectWidth)
+            else if (transform.position.x - screenBounds.x < firstChild.transform.position.x - halfObjectWidth)
             {
                 lastChild.transform.SetAsFirstSibling();
                 lastChild.transform.position = new Vector3(firstChild.transform.position.x - halfObjectWidth * 2, firstChild.transform.position.y, firstChild.transform.position.z);
             }
         }
     }
-    voidLateUpdate()
+    void Update()
     {
-        foreach(GameObject obj in levels)
+
+        Vector3 velocity = Vector3.zero;
+        Vector3 desiredPosition = transform.position + new Vector3(scrollSpeed, 0, 0);
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, 0.3f);
+        transform.position = smoothPosition;
+
+    }
+    void LateUpdate()
+    {
+        foreach (GameObject obj in levels)
         {
             repositionChildObjects(obj);
         }
     }
 }
+
