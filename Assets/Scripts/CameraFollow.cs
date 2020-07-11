4be5ad5b6d6c794536b111b064a320cbc87cd5ab
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2020-07-11 09:12:15 -0400

    added smooth camera for camera follow
    weird hazy effect on the screen when the camera follows, going to try pixel perfect camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,8 +3,10 @@
 public class CameraFollow : MonoBehaviour
 {
     private Transform objectTransform;
-
-    private float deltaX;
+    private float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField] private Vector3 cameraLocation = new Vector3(0, 1, -10);
+    // private float deltaX;
 
     void Start()
     {
@@ -13,7 +15,12 @@ public class CameraFollow : MonoBehaviour
 
     void Update()
     {
-        deltaX = transform.position.x - objectTransform.position.x;
-        transform.position = new Vector3(objectTransform.position.x + deltaX, transform.position.y, transform.position.z);
+        // deltaX = transform.position.x - objectTransform.position.x;
+        //transform.position = Vector3.SmoothDamp(objectTransform.position.x + deltaX, transform.position.y, transform.position.z);
+        Vector3 targetPosition = objectTransform.TransformPoint(cameraLocation);
+
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
     }
 }
