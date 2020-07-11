00fd8f6a063b8ba33d7381205c28ad5cb366ea76
vazeraja <68131084+vazeraja@users.noreply.github.com>
2020-07-11 17:16:09 -0400

    SpaceForce enemies deal damage to player

---
+++ Assets/SpaceForce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceForce : MonoBehaviour
+{
+    public int damage = 30;
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        if(hitInfo.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+            
+        Health health = hitInfo.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(50);
+            
+        }
+    }
+}
