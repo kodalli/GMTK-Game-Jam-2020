f56cc1bb4dce976f3a6859e634952c8dcbddf27d
Yologuri <51071067+Yologuri@users.noreply.github.com>
2020-07-12 08:15:26 -0400

    screenShake

---
+++ Assets/Scripts/screenShake.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class screenShake : MonoBehaviour
+{
+    private float shakeTimeRemaining, shakePower, shakeFadeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            StartShake(0.5f, 0.5f);
+        }
+    }
+    private void LateUpdate()
+    {
+        if(shakeTimeRemaining > 0)
+        {
+            shakeTimeRemaining -= Time.deltaTime;
+
+            float xAmount = Random.Range(-1f, 1f) * shakePower;
+            float yAmount = Random.Range(-1f, 1f) * shakePower;
+
+            transform.position += new Vector3(xAmount, yAmount, 0f);
+
+            shakePower = Mathf.MoveTowards(shakePower, 0f, shakeFadeTime * Time.deltaTime);
+
+        }
+    }
+    public void StartShake(float length, float power)
+    {
+        shakeTimeRemaining = length;
+        shakePower = power;
+
+        shakeFadeTime = power / length;
+
+    }
+}
